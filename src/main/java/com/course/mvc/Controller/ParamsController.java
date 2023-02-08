f7772ea0322cd4bf3c2d0cb840216aa3c73ab422
DesiAPG <desireapgames@gmail.com>
2023-02-07 21:40:46 -0500

    Send params through url

--- src/main/java/com/course/mvc/Controller/ParamsController.java
+++ src/main/java/com/course/mvc/Controller/ParamsController.java
@@ -1,5 +1,6 @@
 package com.course.mvc.Controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,7 +26,20 @@ public class ParamsController {
 
     @GetMapping("/mix-params")
     public String param(@RequestParam String wave, @RequestParam Integer number, Model model) {
-        model.addAttribute("result", "Text sent is: '" + text + "' and the number is '" + number + "'");
+        model.addAttribute("result", "Text sent is: '" + wave + "' and the number is '" + number + "'");
+        return "params/watch";
+    }
+
+    @GetMapping("/mix-params-request")
+    public String param(HttpServletRequest request, Model model) {
+        String wave = request.getParameter("wave");
+        Integer number = null;
+        try {
+            number = Integer.parseInt(request.getParameter("number"));
+        } catch (NumberFormatException e) {
+            number = 0;
+        }
+        model.addAttribute("result", "Text sent is: '" + wave + "' and the number is '" + number + "'");
         return "params/watch";
     }
 }
