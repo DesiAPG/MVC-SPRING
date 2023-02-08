e060142f03bb1888c342042e9e91c89f5a08f558
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-02-08 11:58:59 -0500

    Some changes to parameters in routes

--- src/main/java/com/course/mvc/Controller/PathVariableController.java
+++ src/main/java/com/course/mvc/Controller/PathVariableController.java
@@ -10,10 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/path-variable")
 public class PathVariableController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("title", "Send params from path (@PathVariable)");
+        return "variables/index";
+    }
+
     @GetMapping("/String/{text}")
     public String variablePath(@PathVariable String text, Model model) {
         model.addAttribute("title", "Recive params from path (@PathVariable)");
         model.addAttribute("result", "Text sent is: " + text);
+        return "variables/watch";
+    }
+
+    @GetMapping("/String/{text}/{number}")
+    public String variablePath(@PathVariable String text, @PathVariable Integer number, Model model) {
+        model.addAttribute("title", "Recive params from path (@PathVariable)");
+        model.addAttribute("result", "Text sent is: " + text + " and the number is: '" + number + "'");
         return "variables/watch";
     }
 
